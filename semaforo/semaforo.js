cd0165ac57cb8dea564d9c98994e26fe900f574c
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-01-17 15:29:00 +0000

    Update semaforo.js

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -1,16 +1,15 @@
 const img = document.querySelector("#img");
-const buttons = document.querySelector("#buttons");
+const buttons = document.querySelector("#buttons"); // Buscar o elemento pai dos buttons.
 let colorIndex = 0;
 let intervalId = null;
 
 const trafficLight = (e) => {
-  stopAutomatic();
-  turnOn[e.target.id]();
+  stopInterval()
+  turnOn[e.target.id](); // Vai buscar o id de cada botão, dentro do objeto da função ' turnOn '.
 };
 
 const nextIndex = () => {
-  //colorIndex = colorIndex < 2 ? colorIndex++ : 0;
-  //ou
+  // Vai andar com a imagem 1 por 1.
   if (colorIndex < 2) {
     colorIndex++;
   } else {
@@ -19,21 +18,22 @@ const nextIndex = () => {
 };
 
 const changeColor = () => {
-  const colors = ["red", "yellow", "green"];
-  const color = colors[colorIndex];
-  turnOn[color]();
+  const colors = ["red", "yellow", "green"]; // Array de cores.
+  const color = colors[colorIndex]; // Va buscar o indíce de cada cor um por um.
+  turnOn[color](); // Vai buscar a cor do momento.
   nextIndex();
 };
 
-const stopAutomatic = () => {
+const stopInterval = () => {
   clearInterval(intervalId);
 };
 
 const turnOn = {
+  // Objeto.
   red: () => (img.src = "./img/vermelho.png"),
   yellow: () => (img.src = "./img/amarelo.png"),
   green: () => (img.src = "./img/verde.png"),
-  automatic: () => (intervalId = setInterval(changeColor, 1000)), // Troca a cor a cada 1 segundo.
+  automatic: () => (intervalId = setInterval(changeColor, 1000)), // Vai executar a cada 1s.
 };
 
 buttons.addEventListener("click", trafficLight);
