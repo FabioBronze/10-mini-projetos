2716917f24fb0a73b8573d14c95fbe2ba259e4ba
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-01-22 14:32:04 +0000

    to do list

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,45 +1,74 @@
 "use strict";
 
-let banco = [
-  { tarefa: "Estudar JS", status: "" },
-  { tarefa: "35:03", status: "checked" },
-];
+let banco = [];
 
-const criarItem = (tarefa, status) => {
-  const item = document.createElement("label"); // Cria um label com a classe 'todo__item'
+const getBanco = () => JSON.parse(localStorage.getItem("todoList")) ?? [];
+const setBanco = (banco) =>
+  localStorage.setItem("todoList", JSON.stringify(banco));
+
+const criarItem = (tarefa, status, indice) => {
+  const item = document.createElement("label");
   item.classList.add("todo__item");
   item.innerHTML = `
-        <input type="checkbox" ${status}>
+        <input type="checkbox" ${status} data-indice=${indice}>
         <div>${tarefa}</div>
-        <input type="button" value="X">
+        <input type="button" value="X" data-indice=${indice}>
     `;
-  document.querySelector("#todoList").appendChild(item); // Vai adiconar o item (label) na div com id '#todoList'
+  document.getElementById("todoList").appendChild(item);
 };
 
 const limparTarefas = () => {
-  // Para não repetir o atualizarTela()
-  const todoList = document.querySelector("#todoList");
+  const todoList = document.getElementById("todoList");
   while (todoList.firstChild) {
-    // Enquanto exisitir o primeiro filho
-    todoList.removeChild(todoList.lastChild); // Vai sempre remover o último filho
+    todoList.removeChild(todoList.lastChild);
   }
 };
 
 const atualizarTela = () => {
   limparTarefas();
-  banco.forEach((item) => criarItem(item.tarefa, item.status)); // Varre todo o array, item a item.
+  const banco = getBanco();
+  banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
 };
 
-const inserirItem = (e) => {
-  const tecla = e.key;
-  const texto = e.target.value;
+const inserirItem = (evento) => {
+  const tecla = evento.key;
+  const texto = evento.target.value;
   if (tecla === "Enter") {
+    const banco = getBanco();
     banco.push({ tarefa: texto, status: "" });
+    setBanco(banco);
     atualizarTela();
-    e.target.value = "";
+    evento.target.value = "";
   }
 };
 
-document.querySelector("#newItem").addEventListener("keypress", inserirItem);
+const removerItem = (indice) => {
+  const banco = getBanco();
+  banco.splice(indice, 1);
+  setBanco(banco);
+  atualizarTela();
+};
+
+const atualizarItem = (indice) => {
+  const banco = getBanco();
+  banco[indice].status = banco[indice].status === "" ? "checked" : "";
+  setBanco(banco);
+  atualizarTela();
+};
+
+const clickItem = (evento) => {
+  const elemento = evento.target;
+  console.log(elemento.type);
+  if (elemento.type === "button") {
+    const indice = elemento.dataset.indice;
+    removerItem(indice);
+  } else if (elemento.type === "checkbox") {
+    const indice = elemento.dataset.indice;
+    atualizarItem(indice);
+  }
+};
+
+document.getElementById("newItem").addEventListener("keypress", inserirItem);
+document.getElementById("todoList").addEventListener("click", clickItem);
 
 atualizarTela();
